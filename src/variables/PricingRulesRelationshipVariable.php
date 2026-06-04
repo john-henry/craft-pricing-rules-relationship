4cd8445c93b31ee8be40b0eac40929c388e2017e
John Henry Donovan <info@johnhenry.ie>
2026-06-04 12:43:44 +0100

    php docs

--- src/variables/PricingRulesRelationshipVariable.php
+++ src/variables/PricingRulesRelationshipVariable.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 /**
  * @copyright Copyright (c) John Henry Donovan
  */
@@ -11,12 +9,27 @@ namespace johnhenry\pricingrulesrelationship\variables;
 use johnhenry\pricingrulesrelationship\PricingRulesRelationship;
 use yii\base\InvalidConfigException;
 
+/**
+ * Exposes pricing rules relationship methods to Twig via `craft.pricingRulesRelationship`.
+ *
+ * @author John Henry Donovan <info@johnhenry.ie>
+ * @since 1.0.0
+ */
 class PricingRulesRelationshipVariable
 {
+    // =========================================================================
+    // Public Methods
+    // =========================================================================
+
     /**
+     * Returns the primary-owner IDs of products matching the given pricing rules.
+     *
+     * @param array $selectedSaleIds Catalog pricing rule IDs to match against.
+     * @param bool $hasStock Whether to restrict results to in-stock variants.
+     * @return array<int>
      * @throws InvalidConfigException
      */
-    public function getSaleIds($selectedSaleIds, $hasStock = false): array
+    public function getSaleIds(array $selectedSaleIds, bool $hasStock = false): array
     {
         return PricingRulesRelationship::getInstance()->pricingRulesRelationshipService->getMatchingProductsIds($selectedSaleIds, $hasStock);
     }
